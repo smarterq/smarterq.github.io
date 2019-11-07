c80e25fdeb0ae75675421c8b97aa5b7d15fe3176
七夏浅笑 <i@xhtml.love>
2019-11-07 19:04:51 +0800

    安装cos插件 & 关闭不蒜子
    qcloudcos 已经编写成独立插件
    不蒜子 json 加载太慢，关闭
    lazyload 可自定义图像

--- themes/fluid/scripts/lazyload.js
+++ themes/fluid/scripts/lazyload.js
@@ -1,23 +1,23 @@
 'use strict';
 module.exports.lazyload = function (hexo) {
   var config = hexo.theme.config;
+  var loadingImage = config.lazyload.loadingimg;
   if (!config.lazyload || !config.lazyload.enable) {
     return;
   }
   if (config.lazyload.onlypost) {
     hexo.extend.filter.register('after_post_render', function (data) {
-      data.content = lazyProcess.call(this, data.content);
+      data.content = lazyProcess.call(this, data.content, loadingImage);
       return data;
     });
   } else {
     hexo.extend.filter.register('after_render:html', function (str, data) {
-      return lazyProcess.call(this, str);
+      return lazyProcess.call(this, str, loadingImage);
     });
   }
 };
 
-function lazyProcess(htmlContent) {
-  let loadingImage = '/img/loading.gif';
+function lazyProcess(htmlContent, loadingImage) {
   return htmlContent.replace(/<img(\s*?)src="(.*?)"(.*?)>/gi, (str, p1, p2) => {
     if (/srcset=/gi.test(str)) {
       return str;
